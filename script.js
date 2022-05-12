5ee7fe11cfcffdb5c38b643fb9505c419aa0d23c
Demon <kristyndcb@gmail.com>
2022-05-11 21:50:00 -0600

    New Array created

--- script.js
+++ script.js
@@ -2,24 +2,28 @@
 var timerEl = document.querySelector(".timerDiv");
 var timer;
 var timerCount = 100;
-// ARRAY for each question
-let Q1 = {};
-Q1['question'] = ["Can you answer this?"];
-Q1['choices'] = ['a', 'b', 'c',];
-Q1['correct'] = ['a'];
-console.log(Q1)
+// ARRAY for Question and for choices
+const questions = ['What is the best pet?', 'What is the best fruit?', 'What is the best smell?'];
 
-let Q2 = {};
-Q2['question'] = ['This is a question'];
-Q2['choices'] = ['a', 'b', 'c',];
-Q2['correct'] = ['b'];
-console.log(Q2)
+const choices = ['a)cat', 'b)dog', 'c)fish', 'a)pear', 'b)apple', 'c)peach', 'a)fresh grass', 'b)gasoline', 'c)new leather'];
 
-let Q3 = {};
-Q3['question'] = ['This is a question'];
-Q3['choices'] = ['a', 'b', 'c',];
-Q3['correct'] = ['c'];
-console.log(Q3)
+// let Q1 = {};
+// Q1['question'] = [""];
+// Q1['choices'] = ['a) cat', 'b) dog', 'c) turtle',];
+// Q1['correct'] = ['a'];
+// console.log(Q1)
+
+// let Q2 = {};
+// Q2['question'] = ['This is a question'];
+// Q2['choices'] = ['a', 'b', 'c',];
+// Q2['correct'] = ['b'];
+// console.log(Q2)
+
+// let Q3 = {};
+// Q3['question'] = ['This is a question'];
+// Q3['choices'] = ['a', 'b', 'c',];
+// Q3['correct'] = ['c'];
+// console.log(Q3)
 
 function startTimer() {
   // Sets timer
@@ -30,7 +34,7 @@ function startTimer() {
     if (timerCount === 0) {
       // Clears interval
       clearInterval(timer);
-      alert("Your time is up!")
+      alert("Your time is 🆙 ")
       return;
     }
   }, 1000); //1000 ms = 1second, this is calling this one per second 
@@ -39,11 +43,18 @@ function startTimer() {
 //So i have 3 questions, first i want Q1 to present in #questionSection, when user chooses an answer, i want an alert to pop up if they were correct or not, then i want it to move onto q2...
 
 function startGame() {
-  let displayQ1 = document.querySelector(".displayQuestions");
-  displayQ1.textContent = (Q1['question']);
-  displayQ1.textContent = (Q1['choices[0]']);
-  console.log(Q1['question']);
-  // return displayQ1;
+  var displayQ1 = document.querySelector(".displayQuestions");
+  var liEl1 = document.createElement("li")
+  var liEl2 = document.createElement("li")
+  var liEl3 = document.createElement("li")
+  var textChoice = displayQ1.textContent = (choices[]);
+  displayQ1.textContent = (questions[0]);
+  liEl1.textContent = (textChoice);
+  displayQ1.appendChild(liEl1); 
+  displayQ1.appendChild(liEl2);
+  displayQ1.appendChild(liEl3);
+  console.log(questions[0]);
+  console.log(choices[1]);
 }
 
 //When user hits start button want the timer to begin and the next section to be unhidden--
